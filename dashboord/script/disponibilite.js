070fa2ddcf9f0818e6851bfd18e2d82b292ba828
younesmouqla <younesmouqla03@gmail.com>
2025-11-10 11:30:24 +0100

    added logout for every page

--- dashboord/script/disponibilite.js
+++ dashboord/script/disponibilite.js
@@ -1,6 +1,20 @@
 if (localStorage.getItem("isLoggedIn") !== "true") {   
     window.location.replace("authentification.html");
 }
+
+function afficherFormulaire() {
+      const select = document.getElementById('selectMedecinDispo');
+      const formulaire = document.getElementById('formulaireDispo');
+      
+      if (select.value) {
+        formulaire.style.display = 'block';
+      } else {
+        formulaire.style.display = 'none';
+      }
+    }
+
+
+
 const logout = document.getElementById("logout");
 
 logout.addEventListener("click", function(e) {
