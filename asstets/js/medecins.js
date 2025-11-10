1274e5042223e539dfbfe93e6b37571e39577c50
KacimiLahcen <kacimi.lahcen88@gmail.com>
2025-11-10 14:47:17 +0100

    specialities by js

--- asstets/js/medecins.js
+++ asstets/js/medecins.js
@@ -17,3 +17,27 @@
       `;
       container.appendChild(card);
     });
+
+
+
+    
+
+let array = [];
+
+
+    const storedData = JSON.parse(localStorage.getItem("specialite")) || [];
+    const container2 = document.getElementById("specialiteList");
+
+    array = storedData;
+  // array.forEach(name => addRow(name));
+
+    array.forEach(name => {
+      const btn = document.createElement("button");
+      btn.className = "filter-btn px-6 py-3 rounded-full bg-blue-50 text-blue-700 hover:bg-blue-100 shadow-md font-medium";
+      // btn.innerHTML = `
+      //   <button data-specialty="cardiologie"> ${name}</button>
+      // `;
+btn.textContent = name;
+console.log('test');
+      container2.appendChild(btn);
+    });
